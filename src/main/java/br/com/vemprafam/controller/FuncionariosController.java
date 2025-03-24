95d350d92741c47c2c450e395894706bd6d64693
Gabriel Paiva <paivajosegabriel@gmail.com>
2025-03-24 14:30:55 -0300

    New Form

---
+++ src/main/java/br/com/vemprafam/controller/FuncionariosController.java
@@ -0,0 +1,37 @@
+package br.com.vemprafam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.vemprafam.dao.DaoFuncionario;
+import br.com.vemprafam.pojo.Funcionario;
+
+@Controller
+@RequestMapping("/funcionarios")
+public class FuncionariosController {
+	private DaoFuncionario dao = new DaoFuncionario();
+
+	@GetMapping
+	public String homeEmpty() {
+		return "index";
+	}
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+	@GetMapping("/new")
+	public String showInsertForm(Model model) {
+		model.addAttribute("funcionario", new Funcionario());
+		return "create-funcionario";
+	}
+	@PostMapping
+	public String createFuncionario(@ModelAttribute Funcionario funcionario) {
+		dao.insert(funcionario);
+		return "redirect:/funcionarios";
+	}
+
+}
